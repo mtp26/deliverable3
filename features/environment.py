ca363b7d510f458f36696f036670cd864a325a81
Max Putas <max@duolingo.com>
2014-06-28 14:03:57 -0400

    Initial checkin

---
+++ features/environment.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+def before_all(context):
+    context.browser = webdriver.Firefox()
+
+def after_all(context):
+    context.browser.quit()
