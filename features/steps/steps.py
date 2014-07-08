9166c65a005b450e81652acde21d25c1e9f57471
Max Putas <max@duolingo.com>
2014-07-07 22:51:48 -0400

    Added arguments for checking the cart

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -8,15 +8,20 @@ def step(context):
 def step(context):  
    assert context.browser.title == "Google" 
 
-@when('we add one item to the cart')
+@given(u'the shopping cart is empty')
+def step(context):
+    assert True
+
+@when('we add an item to the cart')
 def step(context):
     context.browser.get('http://www.monoprice.com/Product?c_id=109&cp_id=10904&cs_id=1090407&p_id=10532&seq=1&format=2')
     context.browser.find_element_by_css_selector('.button-yellow-large').click()
 
-@then('the cart should display one item')
-def step(context):
+@then('the page should display that there is {count} item in the cart')
+def step(context, count):
+
     itemCountText = context.browser.find_element_by_id('myBagCount').get_attribute('innerHTML')
 
     # Get the integer value of item count
     itemCount = [int(s) for s in itemCountText.split() if s.isdigit()][0]
-    assert (itemCount == 1)
+    assert (itemCount == int(count))
