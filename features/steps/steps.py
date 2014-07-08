3d96af79c895a46b1a0e9c8c4048adb1152bed19
Max Putas <max@duolingo.com>
2014-07-08 10:54:18 -0400

    CLeanup: removed google items

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,15 +1,16 @@
 import re
 
-@when('we visit google')  
-def step(context):  
-   context.browser.get('http://www.google.com')  
- 
-@then('it should have a title "Google"')  
-def step(context):  
-   assert context.browser.title == "Google" 
+# Helper function to get the item count from
+# the shopping cart of monoprice.com
+def getCartItemCount(context):
+    itemCountText = context.browser.find_element_by_id('myBagCount').get_attribute('innerHTML')
+    # Get the integer value of item count
+    itemCount = [int(s) for s in itemCountText.split() if s.isdigit()][0]
+    return itemCount
 
 @given(u'the shopping cart is empty')
 def step(context):
+    # Clear all of the session cookies, which leaves an empty cart
     context.browser.delete_all_cookies()
     assert True
 
@@ -18,11 +19,6 @@ def step(context):
     context.browser.get('http://www.monoprice.com/Product?c_id=109&cp_id=10904&cs_id=1090407&p_id=10532&seq=1&format=2')
     context.browser.find_element_by_css_selector('.button-yellow-large').click()
 
-@then('the page should display that there is {count} item in the cart')
+@then('the page should display that there is {count} items in the cart')
 def step(context, count):
-
-    itemCountText = context.browser.find_element_by_id('myBagCount').get_attribute('innerHTML')
-
-    # Get the integer value of item count
-    itemCount = [int(s) for s in itemCountText.split() if s.isdigit()][0]
-    assert (itemCount == int(count))
+    assert (getCartItemCount(context) == int(count))
