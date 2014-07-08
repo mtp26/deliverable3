873e637ab83b26de03c45ac962a59254e48c84b8
Max Putas <max@duolingo.com>
2014-07-08 17:26:25 -0400

    Added framework for search testing

--- features/steps/search.py
+++ features/steps/search.py
@@ -1,11 +1,18 @@
 # Check to see if a product page exists for a given item number
 def productPageExists(context, itemNumber):
     itemURL = 'http://www.monoprice.com/Product?p_id=' + itemNumber
-    request = context.browser.get(itemURL)
-    print request.status_code
+    context.browser.get(itemURL)
     return True
 
 @given('item number {itemNumber} is available')
-def step(context):
+def step(context, itemNumber):
     # Make sure that the poduct page for the item exists
-    assert productPageExists(itemNumber)
+    assert productPageExists(context, itemNumber)
+
+@when('a user searches for item number {itemNumber}')
+def step(context, itemNumber):
+    assert True
+
+@then('the page for item number {itemNumber} should be displayed')
+def step(context, itemNumber):
+    assert True
