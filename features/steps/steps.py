dd4efa067f1747e22469337e03d14f6dde806846
Max Putas <max@duolingo.com>
2014-07-08 11:42:26 -0400

    Sanity commit--not working though

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,8 +1,11 @@
 import re
 
-# Helper function to get the item count from
-# the shopping cart of monoprice.com
+# Link to the item that we're going to use for testing
+itemURL = 'http://www.monoprice.com/Product?c_id=109&cp_id=10904&cs_id=1090407&p_id=10532&seq=1&format=2'
+
+# Helper function to get the item count from the shopping cart of monoprice.com
 def getCartItemCount(context):
+    context.browser.get('http://www.monoprice.com/Cart')
     itemCountText = context.browser.find_element_by_id('myBagCount').get_attribute('innerHTML')
     # Get the integer value of item count
     itemCount = [int(s) for s in itemCountText.split() if s.isdigit()][0]
@@ -12,13 +15,30 @@ def getCartItemCount(context):
 def step(context):
     # Clear all of the session cookies, which leaves an empty cart
     context.browser.delete_all_cookies()
-    assert True
 
-@when('we add an item to the cart')
+    # Check just to make sure that it's actually empty
+    assert (getCartItemCount(context) == 0)
+
+@when(u'we add an item to the cart')
 def step(context):
-    context.browser.get('http://www.monoprice.com/Product?c_id=109&cp_id=10904&cs_id=1090407&p_id=10532&seq=1&format=2')
+    context.browser.get(itemURL)
     context.browser.find_element_by_css_selector('.button-yellow-large').click()
 
-@then('the page should display that there is {count} items in the cart')
+@then(u'the page should display that there is {count} items in the cart')
 def step(context, count):
     assert (getCartItemCount(context) == int(count))
+
+@given(u'there is an item in the shopping cart')
+def step(context):
+    # Make sure the cart is empty
+    context.execute_steps(u'given the shopping cart is empty')
+
+    # Add an item to the cart using the previous step definition
+    context.execute_steps(u'when we add an item to the cart')
+
+    # Check just to make sure that there actually is an item in the cart
+    assert (getCartItemCount(context) == 1)
+
+@when(u'we remove 1 item from the cart')
+def step(context):
+    assert False
